10df949f38a771490365e8cc6e640079fee4fb5a
yyh <462985777@qq.com>
2018-11-02 11:33:23 +0800

    初始化

---
+++ src/containers/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+import "common/scss/index.css";
+import "common/scss/flexible.js";
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+     
+    }
+    
+  }
+
+
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default App;
